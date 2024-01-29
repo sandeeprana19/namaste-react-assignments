4fe19a9cf30b2010d87665ee9e3cf1fcdbfac468
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-01-30 00:13:58 +0530

    assigment-01

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
