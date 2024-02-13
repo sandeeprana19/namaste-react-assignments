e1d61f5b6bc4e3d3c8eeec45e8685cc495da8c12
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-02-14 01:40:04 +0530

    assignment-03-accomplished

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Logo from "./logo.svg";
+import User from "./user.jpg";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
+// Coding Assignments 01
+const TitleComponent = () => (
+  <div className="title">
+    <h1 tabIndex="1">Namaste React from h1 🚀</h1>
+    <h2 tabIndex="2">Namaste React from h2 🚀</h2>
+    <h3 tabIndex="3">Namaste React from h3 🚀</h3>
+  </div>
+);
+
+const TitleContainerComponent = () => (
+  <div className="container">
+    {TitleComponent()}
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+  </div>
+);
+
+// Coding Assignments 02
+const HeaderComponent = () => (
+  <header>
+    <a href="#" className="c-image c-image--logo">
+      <img src={Logo} alt="Logo" className="c-image__img" />
+    </a>
+    <input
+      type="search"
+      className="c-input--search"
+      placeholder="Search here"
+    />
+    <a href="#" className="c-image c-image--user">
+      <img src={User} alt="User" className="c-image__img" />
+    </a>
+  </header>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeaderComponent />);
